1ec8b5994d455bd9e26c66a232480f5d43e87e35
Pchela <lyudmilalittlepchela@gmail.com>
2024-08-30 15:12:44 +0300

    Initial Commit

---
+++ chat-app/chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_appApp.swift
+//  chat-app
+//
+//  Created by Лю Пчела on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
