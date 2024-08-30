5d197578156f2a9d9f4447ca663eb8252d673336
Pchela <lyudmilalittlepchela@gmail.com>
2024-08-30 16:35:59 +0300

    message bubble

--- chat-app/ContentView.swift
+++ chat-app/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var messageArray = ["Hi!", "Hello!", "How are u?", "I'm fine, wbu?"]
     var body: some View {
         VStack {
             TitleRow()
+            ScrollView{
+                ForEach(messageArray, id: \.self) { text in
+                    MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+                }
+            }
+            .padding(.top, 10)
+            .background(.white)
         }
         .background(Color("Peach"))
     }
