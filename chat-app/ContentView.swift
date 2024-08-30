e00b88dc6686853ac2b5abb17b12493ad0724ea7
Pchela <lyudmilalittlepchela@gmail.com>
2024-08-30 16:58:22 +0300

    change corner radius to custom

--- chat-app/ContentView.swift
+++ chat-app/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             }
             .padding(.top, 10)
             .background(.white)
+            .cornerRadius(30, corners: [.topLeft, .topRight])
         }
         .background(Color("Peach"))
     }
