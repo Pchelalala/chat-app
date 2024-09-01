744a852082205875352956713103c8896e88d260
Pchela <lyudmilalittlepchela@gmail.com>
2024-09-01 17:02:20 +0300

    set up firebase

--- chat-app/chat_appApp.swift
+++ chat-app/chat_appApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct chat_appApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
